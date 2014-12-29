67ae7764d7087b03bed4285ecc5723808e2a27b7
İsmail Çakır <146213010@stu.kmu.edu.tr>
2014-12-30 00:54:19 +0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,35 @@
+// REQUİRE
+require 'lib/class.youtube.php';
+
+// SINIF BAŞLATMA
+$youtube = new YoutubeSearch();
+
+## PLAYLIST ARAMA
+$data = $youtube->getPlaylistSearch("Eminem",10); // Anahtar Kelime , Limit (max 50)
+
+foreach ($data as $item) {
+    // ÖRNEK GÖSTERİM
+    echo "-> ".$item->title."<br>";
+
+}
+
+## VIDEO ARAMA
+$data = $youtube->getVideoSearch("Rihanna - Diamonds",10); // Anahtar Kelime , Limit (max 50)
+
+foreach ($data as $item) {
+    // ÖRNEK GÖSTERİM
+    echo "-> ".$item->title."<br>";
+
+}
+
+## KANAL ARAMA
+$data = $youtube->getChannelSearch("Michael Jackson",3);
+
+// Json Çıktısı
+var_dump($data);
+
+## VİDEO BİLGİLERİ ÇEKME
+$data = $youtube->getVideoInformation("aDWfF6PSKWM"); // video id'si
+
+// Json Çıktısı
+var_dump($data);
